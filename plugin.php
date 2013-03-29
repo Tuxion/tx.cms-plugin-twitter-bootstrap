4b0e16fac8de3d341e65c906fd2d730d360a3529
Avaq <aldwin.vlasblom@gmail.com>
2013-03-29 19:46:27 +0000

    There it is.

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php tx('Ob')->link('bootstrap'); ?>
+  <!-- CSS Dingen -->
+  <!-- <link href="<?php echo $plugin; ?>src/file.css"> -->
+<?php tx('Ob')->end(); ?>
+
+<?php tx('Ob')->script('bootstrap'); ?>
+  <!-- JS Dingen -->
+  <!-- <script type="text/javascript" src="<?php echo $plugin; ?>src/file.js"></script> -->
+<?php tx('Ob')->end(); ?>
